aef543224bfdb4441da05e49f5d6b5e65373224a
RoiTabach <roi.tabach@gmail.com>
2023-05-02 12:24:53 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analysis of web server access logs',
+    author='Roi Tabach',
+    license='BSD-3',
+)
